7909e409af13f83c07e944b9c64ce36068d0f4c9
Paulo Mulato <pmulato@gmail.com>
2025-05-27 09:05:22 -0300

    Adicionar rota inicial '/' para teste

--- agenda/urls.py
+++ agenda/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("AgendaLembrete online!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),  # <- adiciona isso
 ]
